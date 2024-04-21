c0e6971d8f04a37811b56f8c593c5a8733cc01a8
Onur Serçe <onurserce@gmail.com>
2024-04-21 17:43:57 +0200

    BugFix: output_dir changed to "$output_dir" Change: Default parameter -a changed to 0-10

--- hpc/run_cellpose.sh
+++ hpc/run_cellpose.sh
@@ -2,7 +2,7 @@
 #SBATCH -J run_cellpose
 #SBATCH -o %j_%A_%a.stdout
 #SBATCH -e %j_%A_%a.stderr
-#SBATCH -a 0-100
+#SBATCH -a 0-10
 #SBATCH -t 10:00
 #SBATCH --qos=short
 #SBATCH -c 1
@@ -19,7 +19,7 @@ run_cellpose=${2?Error: no run_cellpose_yaml file given}
 images_dir=${3?Error: no images_dir given}
 
 output_dir="$images_dir"/output/channel_$channel
-mkdir -p output_dir
+mkdir -p "$output_dir"
 
 # $SLURM_ARRAY_TASK_ID will be used as an index to a python script
 python DeepVisualProteomics/hpc/run_cellpose.py "$channel" "$run_cellpose" "$images_dir" "$output_dir" "$SLURM_ARRAY_TASK_ID"
