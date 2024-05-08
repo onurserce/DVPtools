e8ca8949d28f1e683e04cc315d8eb7a6a5339183
Onur Serçe <onurserce@gmail.com>
2024-05-08 14:41:23 +0200

    automated installation script for UNIX based systems

---
+++ install_headless.sh
@@ -0,0 +1,8 @@
+cd "$HOME"/DVPtools
+source "$HOME"/.bashrc
+conda env create -f DVPtools_headless.yaml
+conda activate DVPtools
+pip install -e alphabase/. py-lmd/. SPARCSpy/.
+conda list > conda_list.txt
+pip freeze > pip_freeze.txt
+echo install successful
