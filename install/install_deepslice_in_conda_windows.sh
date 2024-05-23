a645cb3403f7f19e800e2efa0314c2685f2d9720
onurserce <onurserce@gmail.com>
2024-05-24 00:00:22 +0200

    added jupyter

--- install/install_deepslice_in_conda_windows.sh
+++ install/install_deepslice_in_conda_windows.sh
@@ -2,4 +2,4 @@
 conda create -n DeepSlice
 conda activate DeepSlice
 conda install python=3.7
-pip install DeepSlice matplotlib seaborn ipython notebook
+pip install DeepSlice matplotlib seaborn ipython jupyter notebook
