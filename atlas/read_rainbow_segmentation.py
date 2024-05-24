5a79a8b7f7cb9deb5b63b9913b8ddb8466d6fef3
Onur Serçe <onurserce@gmail.com>
2024-05-24 12:00:51 +0200

    init

--- atlas/read_rainbow_segmentation.py
+++ atlas/read_rainbow_segmentation.py
@@ -1,10 +1,27 @@
+import numpy as np
 import pandas as pd
+from imageio.v2 import imread
 
-rainbow_segmentation = pd.read_json('/Users/onur.serce/Downloads/Rainbow 2017.json').set_index('index')
 
-df = rainbow_segmentation
+print(df[df['name'].str.contains('insula')])
 
-df[df['name'].str.contains('insula')]
 
-# Write a function to ask which regions are there in the slice
-# Needs rainbow segmentation mask of the section as an input (and the json look up table)
+def get_segmented_regions(segmentation_mask, mapping_df):
+    # Get unique colors in the segmentation mask
+    unique_colors = np.unique(segmentation_mask.reshape(-1, segmentation_mask.shape[2]), axis=0)
+
+    # Convert the unique colors to a DataFrame
+    unique_colors_df = pd.DataFrame(unique_colors, columns=['red', 'green', 'blue'])
+
+    # Merge the unique colors DataFrame with the mapping DataFrame to get the region names
+    filtered_regions = pd.merge(unique_colors_df, mapping_df, on=['red', 'green', 'blue'], how='inner')
+
+    return filtered_regions
+
+
+# Example usage:
+segmentation_mask = imread('atlas/example_brain/single_slice/brain_pr2_slide33-Rainbow_2017.png')
+mapping_df = pd.read_json('atlas/example_brain/Rainbow 2017.json').set_index('index')
+
+segmented_regions = get_segmented_regions(segmentation_mask, mapping_df)
+print(segmented_regions)
