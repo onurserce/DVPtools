69e16ecf80a2dd848ca0e3cb3259755c1e96addc
Onur Serçe <onurserce@gmail.com>
2024-07-05 17:03:38 +0200

    1) parameterized the raw_file_format 2) added a hint to warn the user that the diann execution command is diann-1.8.1 on the HPC

--- diann_command.py
+++ diann_command.py
@@ -1,18 +1,17 @@
-"diann.exe --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_25_shape_0.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_25_shape_1.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_25_shape_2.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_50_shape_0.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_50_shape_1.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_50_shape_2.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_125_shape_0.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_125_shape_1.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_125_shape_2.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_250_shape_0.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_250_shape_1.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_250_shape_2.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_500_shape_0.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_500_shape_1.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_500_shape_2.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_1000_shape_0.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_1000_shape_1.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_1000_shape_2.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_blank_0.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_blank_1.raw  --f C:\Users\onur.serce\Downloads\2024May_Onur\20240504_OA4_33min_EdRo_SA_ER586_Onur_Input_Experiment_slideB_blank_2.raw  --lib  --threads 32 --verbose 4 --out C:\Users\onur.serce\Downloads\2024May_Onur\diann output main\report.tsv --qvalue 0.01 --matrices --out-lib C:\Users\onur.serce\Downloads\2024May_Onur\diann output main\report-lib.tsv --gen-spec-lib --predictor --fasta C:\Users\onur.serce\ownCloud - Onur Serce (onse)@datashare.mpcdf.mpg.de\UP000000589_10090.fasta --fasta-search --min-fr-mz 200 --max-fr-mz 1800 --met-excision --cut K*,R* --missed-cleavages 1 --min-pep-len 6 --max-pep-len 32 --min-pr-mz 300 --max-pr-mz 1800 --min-pr-charge 1 --max-pr-charge 6 --unimod4 --var-mods 1 --double-search --individual-mass-acc --individual-windows --reanalyse --relaxed-prot-inf --smart-profiling --pg-level 1 --peak-center --no-ifs-removal"
-
 import os
 
 
-def generate_diann_command(folder, output_folder, fasta_file, threads=32, qvalue=0.01, min_fr_mz=200, max_fr_mz=1800,
+def generate_diann_command(folder, output_folder, fasta_file, raw_file_format='.raw', threads=32, qvalue=0.01,
+                           min_fr_mz=200, max_fr_mz=1800,
                            min_pr_mz=300, max_pr_mz=1800, min_pr_charge=1, max_pr_charge=6, min_pep_len=6,
                            max_pep_len=32,
                            missed_cleavages=1):
     # Initialize the command with the executable and options
-    command = 'diann.exe'
+    command = 'diann.exe'  # This is diann-1.8.1 on the HPC
 
     # Add .raw files from the specified folder
     for file_name in os.listdir(folder):
-        if file_name.endswith('.raw'):
+        if file_name.endswith(raw_file_format):
             command += f' --f {os.path.join(folder, file_name)}'
 
     # Add other options
